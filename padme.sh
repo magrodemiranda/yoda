c63cad2c14d98ca00a6b00326cf7430df2ba9e0b
Leonardo Miranda <Leonardo.Miranda@123.com>
2018-06-19 23:09:34 +0000

    Adicao do fliget

--- padme.sh
+++ padme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+fliget "PADME.sh"
 for IP in 10 20 30;
 do
-	ping -c 2 192.168.99.$IP >- && echo "Com Conexao" || echo "Sem Conexao";
+	ping -c2 192.168.99.$IP >- && echo "Com Conexao" || echo "Sem Conexao";
 done
 
